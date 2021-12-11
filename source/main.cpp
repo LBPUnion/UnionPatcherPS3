fee112d238b213aeac62f3f866f29ef9b7258b9d
Logan Lowe <loganr.lowe@gmail.com>
2021-12-11 15:15:59 -0700

    No more switch statement
    That's really messy and I don't like it, now I'm storing ALL title IDs and their friendly titles in a map. I still need to check directories for the presence of an EBOOT.BIN but it's a start

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,21 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <string>
 #include <malloc.h>
+#include <string>
+#include <sstream>
+#include <map>
+
+// There's a bug in the standard library that breaks std::to_string LMAO here's my fix
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
 
 // psl1ght stuff
 #include <io/pad.h>
@@ -14,6 +27,35 @@
 
 // local thing - rsxutil does basic boilerplate stuff that I don't want to worry about
 #include "rsxutil.h"
+
+// Map of all LBP Game IDs to title names. This is really important for us to scan for installations and reflect it in the UI
+const std::map<std::string, std::string> gameIDRealNames = 
+{
+	// LBP1
+	{"BCUS98148", "LittleBigPlanet™ [US] [Disc]"},
+	{"BCES00141", "LittleBigPlanet™ [EU] [Disc]"},
+	{"NPUA80472", "LittleBigPlanet™ [US] [PSN]"},
+	{"NPEA00241", "LittleBigPlanet™ [EU] [PSN]"},
+	{"BCUS98208", "LittleBigPlanet™ Game of the Year Edition [US] [Disc]"},
+	{"BCES00611", "LittleBigPlanet™ Game of the Year Edition [EU] [Disc]"},
+
+	// LBP2
+	{"BCUS98245", "LittleBigPlanet™ 2 [US] [Disc]"},
+	{"BCES01086", "LittleBigPlanet™ 2 [EU] [Disc]"},
+	{"BCES00850", "LittleBigPlanet™ 2 [EU] [Disc]"},
+	{"BCAS20113", "LittleBigPlanet™ 2 [AS] [Disc]"},
+	{"NPUA80662", "LittleBigPlanet™ 2 [US] [PSN]"},
+	{"NPEA00324", "LittleBigPlanet™ 2 [EU] [PSN]"},
+	{"BCUS98372", "LittleBigPlanet™ 2: Special Edition [US] [Disc]"},
+	{"BCES01693", "LittleBigPlanet™ 2 Extras Edition [EU] [Disc]"},
+	{"BCES01694", "LittleBigPlanet™ 2 Extras Edition [EU] [Disc]"},
+
+	// LBP3
+	{"BCUS98362", "LittleBigPlanet™ 3 [US] [Disc]"},
+	{"BCES02068", "LittleBigPlanet™ 3 [EU] [Disc]"},
+	{"BCES01663", "LittleBigPlanet™ 3 [EU] [Disc]"},
+	{"NPUA81116", "LittleBigPlanet™ 3 [US] [PSN]"}
+};
 
 // Store the value of the button pressed so we can do stuff with it in code
 static vs32 dialog_action = 0;
@@ -60,7 +102,7 @@ static void sysutil_exit_callback(u64 status,u64 param,void *usrdata)
 }
 }
 
-// Check for new events (calls pending callback function is my best way of trying to understand this)
+// Check for new events (checks pending callback functions is my best way of trying to understand this)
 static void do_flip()
 {
 	sysUtilCheckCallback();
@@ -69,10 +111,10 @@ static void do_flip()
 
 // Hash char arrays so I can compare them. This is an absolutely disgusting hack but this is my first ever writing stuff in C++
 // and this one guy who thought this up had lots of upvotes on Stackoverflow.
-constexpr unsigned int hash(const char *s, int off = 0) {                        
-    return !s[off] ? 5381 : (hash(s, off+1)*33) ^ s[off];                           
-}  
-
+constexpr unsigned int hash(const char *s, int off = 0)
+{                        
+    return !s[off] ? 0 : (hash(s, off+1)*33) ^ s[off];                           
+}
 
 int main(int argc,char *argv[])
 {
@@ -86,8 +128,11 @@ int main(int argc,char *argv[])
 	sysFSDirent dir;
 	size_t read;
 
-	// Boolean for keeping track of whether or not an installation was found, once I actually start keeping track of which versions are installed I can just see if I recorded any and this will be useless why did I put this here?????
-	bool foundInstallation = false;
+	// String for storing all of the names of different installations found on internal storage
+	std::string installationsFriendlyNames = "";
+
+	// Integer for tracking number of installations so I can reflect that in the UI (lol) and actually check to see if I find ANY installations
+	int installationsCount;
 
 	// Initialize 1mb buffer in system memory for RSX (i think)
  	void *host_addr = memalign(1024*1024,HOST_SIZE);
@@ -119,90 +164,18 @@ int main(int argc,char *argv[])
 				printf(dir.d_name);	
 				printf(" - checking to see if it's an LBP installation...");
 
-				// Test directory name against known LBP Game IDs. I SWEAR I THIS WONT STAY A SWITCH STATEMENT... MAYBE (it's certainly pragmatic even if a loop looks nicer)
-				switch (hash(dir.d_name))
-				{
-					// LBP1
-					case hash("BCUS98148"): // LittleBigPlanet™ [US] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES00141"): // LittleBigPlanet™ [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("NPUA80472"): // LittleBigPlanet™ [US] [PSN]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("NPEA00241"): // LittleBigPlanet™ [EU] [PSN]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCUS98208"): // LittleBigPlanet™ Game of the Year Edition [US] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES00611"): // LittleBigPlanet™ Game of the Year Edition [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
+						if(gameIDRealNames.count(patch::to_string(dir.d_name)) > 0)
+						{
+							printf(" Yes!\n");
+							installationsCount++;
+							installationsFriendlyNames += gameIDRealNames.at(patch::to_string(dir.d_name)) + "\n";
+						}
+						else
+						{
+							printf(" No!\n");
+						}
 
-					// LBP2
-					case hash("BCUS98245"): // LittleBigPlanet™ 2 [US] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES01086"): // LittleBigPlanet™ 2 [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES00850"): // LittleBigPlanet™ 2 [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCAS20113"): // LittleBigPlanet™ 2 [AS] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("NPUA80662"): // LittleBigPlanet™ 2 [US] [PSN]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("NPEA00324"): // LittleBigPlanet™ 2 [EU] [PSN]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCUS98372"): // LittleBigPlanet™ 2: Special Edition [US] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES01693"): // LittleBigPlanet™ 2 Extras Edition [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
 
-					//LBP3
-					case hash("BCUS98362"): // LittleBigPlanet™ 3 [US] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES02068"): // LittleBigPlanet™ 3 [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("BCES01663"): // LittleBigPlanet™ 3 [EU] [Disc]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					case hash("NPUA81116"): // LittleBigPlanet™ 3 [US] [PSN]
-						printf(" Yes!\n");
-						foundInstallation = true;
-						break;
-					default: // Every other title that is not a variation of LittleBigPlanet
-						printf(" No!\n");
-						break;
-				}
 				// keep checking for events
 				do_flip();
 			}
@@ -211,11 +184,13 @@ int main(int argc,char *argv[])
         }
 
 	// Prompt the user depending on whether or not any LBP installations were found on internal storage
-	if(foundInstallation)
+	if(installationsCount > 0)
 	{
 		printf("Matching LBP installation found! :)\n");
+		printf(installationsFriendlyNames.c_str());
 		dialogType = (msgType)(MSG_DIALOG_NORMAL | MSG_DIALOG_BTN_TYPE_OK | MSG_DIALOG_DISABLE_CANCEL_ON);
-		msgDialogOpen2(dialogType,"UnionPatcher found the following installations on your internal storage:\n\nTODO: store installations properly so we can show them and actually patch them\n\nDo you want to continue?",dialog_handler,NULL,NULL);
+		std::string message = "UnionPatcher found the following installations on your internal storage:\n\n" + installationsFriendlyNames + "\nDo you want to continue?";
+		msgDialogOpen2(dialogType, message.c_str(), dialog_handler,NULL,NULL);
 	}
 	else
 	{
